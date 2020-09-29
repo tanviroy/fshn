a43fb6545aecf4e45c384576f0c47179792f01fd
hulikalruthu <32595493+hulikalruthu@users.noreply.github.com>
2020-09-29 19:43:45 +0530

    Update Cart.js
    Adds Parallax with infinite scroll to cart site. Adds grid displaying items in cart and mouse tracker to control scroll speed.

--- frontend/src/Pages/Cart.js
+++ frontend/src/Pages/Cart.js
@@ -1,11 +1,41 @@
 // This is the Cart Page - will show products (if any) that are in the cart
 
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "./App.scss";
 
 export default function Cart() {
-    return (
-        <div>
-            <h1>This is the Cart Page</h1>
+    const [offsetY, setOffsetY] = useState(0);
+    const handleScroll = () => setOffsetY(window.pageYOffset);
+
+    useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
+
+    const renderContent = () => (
+    <>
+        <div className="Parallax__content__heading">
+        <h1 className="Parallax__content__heading__text">Alta Romea</h1>
+        <h2 className="Parallax__content__heading__caption">
+        You have no items in your shopping cart.
+        Return home to continue shopping.
+        </h2>
         </div>
+    </>
+    );
+
+    return (
+    <section className="Parallax">
+        <div
+        className="Parallax__background"
+        style={{ transform: `translateY(-${offsetY * 0.5}px)` }}
+        />
+        <div
+        className="Parallax__background-triangles"
+        style={{ transform: `translateY(${offsetY * 0.8}px)` }}
+        />
+        <div className="Parallax__content">{renderContent()}</div>
+    </section>
     );
 }
