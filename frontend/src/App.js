ed81dcf4f6e11b0ef51acf849b318cd773a0d1f2
Soham De <soham@Sohams-MacBook-Pro.local>
2020-10-07 10:02:24 +0530

    Banner and Logo Modified

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
-// This is the main file containing the core of the application. 
-// It holds major routes and renders pages as components. 
+// This is the main file containing the core of the application.
+// It holds major routes and renders pages as components.
 
-import React, {Component} from "react";
+import React, { Component } from "react";
 import { Route, BrowserRouter } from "react-router-dom";
 import "./App.css";
 import Home from "./Pages/Home";
@@ -13,27 +13,22 @@ import TnC from "./Pages/T&C";
 import NavbarComp from "./components/navbar";
 import FooterComp from "./components/footer";
 
-
 class App extends Component {
-
   render() {
-
     return (
+      <BrowserRouter>
+        <NavbarComp />
 
-        <BrowserRouter>
+        <Route path="/" exact component={Home} />
+        <Route path="/cart" component={Cart} />
+        <Route path="/login" component={Login} />
+        <Route path="/profile" component={Profile} />
+        <Route path="/terms-and-conditions" component={TnC} />
+        <Route path="/about-us" component={TnC} />
+        <Route path="/contact-us" component={TnC} />
 
-          <NavbarComp />
-            
-            <Route path="/" exact component={Home} />
-            <Route path="/cart" component={Cart} />
-            <Route path="/login" component={Login} />
-            <Route path="/profile" component={Profile} />
-            <Route path="/terms-and-conditions" component={TnC} />
-
-          <FooterComp />
-          
-        </BrowserRouter>
-
+        <FooterComp />
+      </BrowserRouter>
     );
   }
 }
--- frontend/src/components/products.js
+++ frontend/src/components/products.js
@@ -7,27 +7,38 @@ import { Link } from "react-router-dom";
 const ProductsComp = ({ products }) => {
   return (
     <div>
-    <center><h1>Featured Products</h1></center>
+      <center>
+        <h1> ... </h1>
+        <h1>Featured Products</h1>
+      </center>
 
-    <div className="products-container">
-          {products.map((product) => (
-              <li key={product._id}>
-                  <div className="product">
-                  <div className="container">
-                  <Link to={"/product/" + product._id}><img className="product-image" src={product.image} alt="product"/></Link>
-                  </div>
-                  <div className="product-name">
-                      <Link to={"/product/" + product._id}>{product.name}</Link>
-                  </div>
-                  <div className="product-brand">{product.brand}</div>
-                  <div className="product-price">${product.price}</div>
-                  <div className="product-rating">{product.rating} Stars ({product.numReviews} Reviews)</div>
-                  </div>
-              </li>
-          ))}
+      <div className="products-container">
+        {products.map((product) => (
+          <li key={product._id}>
+            <div className="product">
+              <div className="container">
+                <Link to={"/product/" + product._id}>
+                  <img
+                    className="product-image"
+                    src={product.image}
+                    alt="product"
+                  />
+                </Link>
+              </div>
+              <div className="product-name">
+                <Link to={"/product/" + product._id}>{product.name}</Link>
+              </div>
+              <div className="product-brand">{product.brand}</div>
+              <div className="product-price">${product.price}</div>
+              <div className="product-rating">
+                {product.rating} Stars ({product.numReviews} Reviews)
+              </div>
+            </div>
+          </li>
+        ))}
+      </div>
     </div>
-    </div>
-  )
+  );
 };
 
-export default ProductsComp
+export default ProductsComp;
