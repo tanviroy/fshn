9090662dabc8e292ae30b3081ab8a4b593766443
Tanvi Roy <tanvi.roy_ug22@ashoka.edu.in>
2020-10-23 17:06:14 +0530

    o no

--- backend/server.js
+++ backend/server.js
@@ -659,11 +659,9 @@ app.get("/", (req, res) => {
 
 //========================================= SERVER STARTING
 
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('../frontend/build'));
+}
 
 app.listen(PORT, () => {
   console.log(`Server started at http://localhost:${PORT}`);
